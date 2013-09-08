26091c0a541d1d60f2784a40de13f0483d8340df
wallstop <dbfsinfil@gmail.com>
2013-09-07 23:31:01 -0400

    Update Templates.cpp
    Updated with more comments and fleshed out methods.

--- Templates.cpp
+++ Templates.cpp
@@ -1,13 +1,26 @@
 // Here, "typename" can either be "typename" or "class", depending 
 //	on how specific you want the template to be.
+//	
+// The "T" here is completely arbitrary. For general "generic" classes,
+// "T" is a perfectly fine name. More specific names help readability,
+// such as:
+//	template <typename numericType>
+// or:
+//	template <typename containerType>
+// These names would be used inside the function for type specifity.
+//	containerType bar(const containerType& input);
 template <typename T>
 class Foo
 {
 public:
-	Foo();
-	~Foo();
+	Foo(){}
+	~Foo(){}
 	
-	T bar(const T& input);
+	T bar(const T& input)
+	{
+		input = m_var;
+		return m_var;
+	}
 	
 private:
 	T m_var;
